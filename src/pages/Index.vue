f351cbe5d9bb3d701289068c82ea3a4f925111f7
Peter <griffitp12@gmail.com>
2021-10-19 18:04:18 +0900

    Nice, making a connection to the back!

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,14 +3,17 @@
   loading...
 </div>
   <q-page v-else class="row items-center justify-evenly">
-    Loaded!
+    {{wineState}}
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import { setAllWines } from '../global/store/setters';
+import { AccessWineStore } from 'src/global/store/wineStore';
+
  const isLoading = ref(true)
+ const wineState = AccessWineStore()
 
  const loadWines = () => {
    setAllWines().catch((err) => alert(err))
