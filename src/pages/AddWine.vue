cb4c9f7254aae2ade89e1c9815d41429ae9826db
Peter <griffitp12@gmail.com>
2021-10-22 10:01:33 +0900

    cleaned up styling on AddWine

--- src/pages/AddWine.vue
+++ src/pages/AddWine.vue
@@ -1,39 +1,79 @@
 <template>
-  <div v-if="!winePosted" class="q-pa-md" style="max-width: 400px">
-    <q-card>
-      <q-toggle v-model="nameToggle" :label="nameToggle" true-value="I know the wine's name" false-value="I don't know the wine's name" />
-      <q-toggle v-model="vintageToggle" :label="vintageToggle" true-value="I know the wine's vintage" false-value="I don't know the wine's vintage" />
-      <q-toggle v-model="varietalToggle" :label="varietalToggle" true-value="I know the wine's varietal" false-value="I don't know the wine's varietal" />
-    </q-card>
-    <q-form @submit="submitHandler" class="q-gutter-md">
-      <q-input v-if="nameToggle === `I know the wine's name`" filled v-model="wine_name" label="Wine Name" hint="What does it say on the bottle?" />
-      <q-input v-if="vintageToggle === `I know the wine's vintage`"
-        filled
-        v-model="vintage"
-        label="Vintage"
-        hint="The year the wine was pressed"
-        maxlength="4"
-        lazy-rules
-        :rules="[numberRuleOne]"
-      />
-
-      <q-input v-if="varietalToggle === `I know the wine's varietal`" filled v-model="varietal" label="Varietal" hint="Cabernet, Chardonnay, etc." />
-      <q-input filled v-model="color" label="Wine Color *" hint="Red, White, Sparkling, etc." lazy-rules :rules="[colorRule]" />
-
-      <div>
-        <q-btn label="Submit" type="submit" color="primary" />
+  <q-card v-if="!winePosted" class="q-ma-xl q-pa-md">
+    <div class="fit row justify-center">
+      <div class="q-pa-sm column col-auto">
+        <q-toggle
+          v-model="nameToggle"
+          :label="nameToggle"
+          true-value="I know the wine's name"
+          false-value="I don't know the wine's name"
+        />
+        <q-toggle
+          v-model="vintageToggle"
+          :label="vintageToggle"
+          true-value="I know the wine's vintage"
+          false-value="I don't know the wine's vintage"
+        />
+        <q-toggle
+          v-model="varietalToggle"
+          :label="varietalToggle"
+          true-value="I know the wine's varietal"
+          false-value="I don't know the wine's varietal"
+        />
       </div>
-    </q-form>
-  </div>
+    </div>
+    <div class="fit row justify-center">
+      <q-form @submit="submitHandler" class="col-7 q-pa-lg">
+        <q-input
+          v-if="nameToggle === `I know the wine's name`"
+          class="q-my-lg"
+          filled
+          v-model="wine_name"
+          label="Wine Name"
+          hint="What does it say on the bottle?"
+        />
+        <q-input
+          v-if="vintageToggle === `I know the wine's vintage`"
+          class="q-my-lg"
+          filled
+          v-model="vintage"
+          label="Vintage"
+          hint="The year the wine was pressed"
+          maxlength="4"
+          lazy-rules
+          :rules="[numberRuleOne]"
+        />
+
+        <q-input
+          v-if="varietalToggle === `I know the wine's varietal`"
+          class="q-my-lg"
+          filled
+          v-model="varietal"
+          label="Varietal"
+          hint="Cabernet, Chardonnay, etc."
+        />
+        <q-input
+          class="q-my-lg"
+          filled
+          v-model="color"
+          label="Wine Color *"
+          hint="Red, White, Sparkling, etc."
+          lazy-rules
+          :rules="[colorRule]"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+        </div>
+      </q-form>
+    </div>
+  </q-card>
+
   <div v-else>
     You've successfully posted a wine! Would you like to post another?
     <q-btn to="/AddWine" @click="winePosted = false"> Post Another Wine</q-btn>
   </div>
-  <AddWineDialog
-    v-model="showConfirmDialog"
-    :newWine="newWine"
-    @posted="winePosted = true"
-  />
+  <AddWineDialog v-model="showConfirmDialog" :newWine="newWine" @posted="winePosted = true" />
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
@@ -49,16 +89,16 @@ const winePosted = ref(false);
 const nameToggle = ref("I know the wine's name");
 const vintageToggle = ref("I know the wine's vintage");
 const varietalToggle = ref("I know the wine's varietal");
-const newWine: NewWine = {}
+const newWine: NewWine = {};
 
 const colorRule = (val: string) => !!val || `Even if you don't know anything else about the wine, surely you know the color?`;
 const numberRuleOne = (val: string) => /^(?:19|20)\d{2}$/.test(val) || 'Please enter four digit year beginning with 19 or 20';
 
 const submitHandler = () => {
-  if (nameToggle.value === `I know the wine's name`) newWine.name = wine_name.value
-  if (vintageToggle.value === `I know the wine's vintage`) newWine.vintage = vintage.value
-  if (varietalToggle.value === `I know the wine's varietal`) newWine.varietal = varietal.value
-  newWine.color = color.value
-  showConfirmDialog.value = true
-}
+  if (nameToggle.value === `I know the wine's name`) newWine.name = wine_name.value;
+  if (vintageToggle.value === `I know the wine's vintage`) newWine.vintage = vintage.value;
+  if (varietalToggle.value === `I know the wine's varietal`) newWine.varietal = varietal.value;
+  newWine.color = color.value;
+  showConfirmDialog.value = true;
+};
 </script>
